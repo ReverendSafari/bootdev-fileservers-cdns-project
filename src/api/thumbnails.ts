7781e71a1a1244e2f741d39f2f388a9e58cddc5c
Safari <s.swanson24@ncf.edu>
2025-10-07 11:38:04 -0400

    finsihed
    c
    
    git commit
    # On macOS and Linux.
    curl -LsSf https://astral.sh/uv/install.sh | sh# On macOS and Linux.
    curl -LsSf https://astral.sh/uv/install.sh | sh
    ''
    '

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,43 +1,25 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "path";
+import { randomBytes } from "crypto";
+
 
 type Thumbnail = {
   data: ArrayBuffer;
   mediaType: string;
 };
 
-const videoThumbnails: Map<string, Thumbnail> = new Map();
 
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
 
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
   }
@@ -47,7 +29,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const thumbnail = formData.get("thumbnail");
 
-  return respondWithJSON(200, null);
+  if (!(thumbnail instanceof File)) {
+    throw new BadRequestError("Thumbnail not a file");
+  }
+
+  if (thumbnail.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Error file too large, must be under 10MB");
+  }
+
+  const mediaType = thumbnail.type;
+
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type");
+  }
+
+  const fileExtension = `.${mediaType.split('/')[1]}`;
+  const arrayBuffer = await thumbnail.arrayBuffer();
+  const randomVideoIdString = randomBytes(32).toString('base64url');
+  const filePath = path.join(cfg.assetsRoot, randomVideoIdString + fileExtension);
+
+  Bun.write(filePath, arrayBuffer);
+  const video = getVideo(cfg.db, videoId);
+
+  if (video?.userID != userID) {
+    throw new UserForbiddenError("UserID does not match UserID of video");
+  }
+
+  video.thumbnailURL = `http://localhost:${cfg.port}/assets/${randomVideoIdString}${fileExtension}`;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,109 @@
 import { respondWithJSON } from "./json";
-
+import { randomBytes } from "crypto";
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { file, S3Client, type BunRequest } from "bun";
+import { BadRequestError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo, type Video } from "../db/videos";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const videoSizeLimit = 1 << 30;
+  const UUID_RE = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+  const params = req.params as { videoId?: string};
+  const videoId = params.videoId;
+
+  if (!videoId || !UUID_RE.test(videoId)) {
+    throw new BadRequestError("Invalid or Missing video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const videoRecord = getVideo(cfg.db, videoId);
+
+  if (videoRecord?.userID !== userID) {
+    throw new UserForbiddenError("Provided user is not owner of video");
+  }
+
+  const formData = await req.formData();
+  const video = formData.get("video")
+
+  if (!(video instanceof File)) {
+    throw new BadRequestError("Video is not a file");
+  }
+
+  if (video.size > videoSizeLimit) {
+    throw new BadRequestError("Video exceeds upload size limit");
+  }
+
+  if (video.type !== "video/mp4") {
+    throw new BadRequestError("Invalid file type, not an mp4");
+  }
+
+  const fileKey = randomBytes(32).toString('hex');
+  const s3Key = `${fileKey}.mp4`;
+  const tmpFile = Bun.file(`/tmp/${s3Key}`);
+
+  await Bun.write(`/tmp/${s3Key}`, video);
+  
+  const aspectRatio = await getVideoAspectRatio(`/tmp/${s3Key}`);
+  const fullKey = `${aspectRatio}/${s3Key}`; 
+  const processedPath = await processVideoForFastStart(`/tmp/${s3Key}`);
+  const processedVideoFile = Bun.file(processedPath);
+
+  const s3File = cfg.s3Client.file(fullKey);
+  await s3File.write(processedVideoFile, { type: video.type } );
+
+  videoRecord.videoURL = `${cfg.cloudfrontUrl}${fullKey}`;
+  updateVideo(cfg.db, videoRecord);
+
+  await tmpFile.delete();
+  await processedVideoFile.delete();
+
+  return respondWithJSON(200, videoRecord);
+}
+
+function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
+  const presignedUrl = cfg.s3Client.presign(key, {expiresIn: expireTime});
+  return presignedUrl;
+}
+
+
+
+export async function getVideoAspectRatio(filePath: string) {
+  const command_arr = ['ffprobe', '-v', 'error', '-print_format', 'json', '-show_streams', `${filePath}`];
+  const process = Bun.spawn(command_arr);
+
+  const exitCode = await process.exited;
+
+  if (exitCode !== 0) {
+    throw new Error("Error while spawning new bun process");
+  }
+  
+  const stdout = await new Response(process.stdout).json();
+  const width = stdout.streams[0].width;
+  const height = stdout.streams[0].height;
+
+  if (!width || !height) {
+    throw new Error("Error extracting video dimensions, object missing fields");
+  }
+
+  if (Math.floor(width / 16) == Math.floor(height / 9)) {
+    return 'landscape';
+  }
+
+  if (Math.floor(width / 9) == Math.floor(height / 16)) {
+    return 'portrait';
+  }
+
+  return 'other';
+}
+
+async function processVideoForFastStart(inputFilePath: string) {
+  const outPath = inputFilePath + '.proccessed';
+  const command_arr = ['ffmpeg', '-i', `${inputFilePath}`, '-movflags', 'faststart', '-map_metadata', '0', '-codec', 'copy', '-f', 'mp4', `${outPath}`];
+  const process = Bun.spawn(command_arr);
+
+  await process.exited;
+  return outPath;
 }
